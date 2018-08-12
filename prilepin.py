309a452e6796a2d54f3842f3fe8d0f8fbb38a9f8
AlexAksentyev_Mac <alexaksentyev@gmail.com>
2018-08-12 18:51:01 +0200

    improved output convenience; began Micro (cycle) class

--- prilepin.py
+++ prilepin.py
@@ -48,10 +48,11 @@ class Session:
                            dtype = [('pct', float), ('sets', int), ('reps', int)])
     WEEK = None # defined in children
     
-    def __init__(self, prim_lift, sec_lift, targ_inol=1.5):
+    def __init__(self, week_day, prim_lift, sec_lift, targ_inol=1.5):
         #targ_inol=1.5(2) for loading phases, .75(1) for maintenance / session
         #targ inol 3 for loading 2 for maintenance / week
         # https://drive.google.com/file/d/1Ob-s7dPK63Or_lNK-FPvvxk4NeMjkJKv/view
+        self._week_day = week_day
         self._prim_lift = prim_lift
         self._sec_lift = sec_lift
         self._targ_inol = targ_inol
@@ -74,7 +75,7 @@ class Session:
         set_vols = np.round(vols/sets)
         return pcts, wgts, sets, reps, inols, vols, set_inols, set_vols
 
-    def week(self, week_num):            
+    def week(self, week_num, silent=False):            
         work_sets = self.WEEK[self.WEEK['WN']==week_num][['pct', 'sets', 'reps']]
         warmup_sets = self.WRM[self.WRM['pct']<work_sets['pct']]
 
@@ -97,11 +98,13 @@ class Session:
         sec_tbl = np.array(list(zip(sec_pct, sec_wgt, ssets, sreps, sinol, svol, ssinol, ssvol)), dtype=self._typespec)
         sec_vol = np.sum(sec_tbl['sets']*sec_tbl['reps']*sec_tbl['wgt'])
 
-        print('PRIMARY ({}); TOT INOL: {:4.2f}; TOT VOL: {:4.0f} [kgs]'.format(pri.name, pri_inol, pri_vol))
-        print(pds.DataFrame(pri_tbl, index=[week_num]*len(pri_tbl)))
-        print('SECONDARY ({}); TOT INOL: {:4.2f}; TOT VOL: {:4.0f} [kgs]'.format(sec.name, sec_inol, sec_vol))
-        print(pds.DataFrame(sec_tbl, index=[week_num]*len(sec_tbl)))
-        print('TOT INOL: {:4.2f}; TOT VOL: {:4.0f}'.format(pri_inol+sec_inol, pri_vol+sec_vol))
+        if not silent:
+            print('        '+self._week_day)
+            print('PRIMARY ({}); TOT INOL: {:4.2f}; TOT VOL: {:4.0f} [kgs]'.format(pri.name, pri_inol, pri_vol))
+            print(pds.DataFrame(pri_tbl, index=[week_num]*len(pri_tbl)))
+            print('SECONDARY ({}); TOT INOL: {:4.2f}; TOT VOL: {:4.0f} [kgs]'.format(sec.name, sec_inol, sec_vol))
+            print(pds.DataFrame(sec_tbl, index=[week_num]*len(sec_tbl)))
+            print('TOT INOL: {:4.2f}; TOT VOL: {:4.0f}'.format(pri_inol+sec_inol, pri_vol+sec_vol))
         return dict(pri=pri_tbl, sec=sec_tbl)
         
 class MaxEffDay(Session):
@@ -121,10 +124,37 @@ class DynEffDay(Session):
                          (6, .70, 3, 6),
                          (7, .75, 3, 6)],
                             dtype=[('WN', int), ('pct', float), ('sets', int), ('reps', int)])
+
+class Micro:
+    def __init__(self, max_eff_days, dyn_eff_days):
+        self.max_eff_days = max_eff_days
+        self.dyn_eff_days = dyn_eff_days
+
+    def volume(self, week_num):
+        typ = list(zip(['pri','sec','net'], [int]*3))
+        max_eff_vol = np.empty(len((self.max_eff_days), 2), dtype=typ)
+        dyn_eff_vol = np.empty(len((self.dyn_eff_days), 2), dtype=typ)
+        for i, day in enumerate(self.max_eff_days):
+            dm = day.week(week_num, silent=True)
+            pv = dm['pri']['vol'].sum()
+            sv = dm['sec']['vol'].sum()
+            max_eff_vol[i] = pv, sv, pv+sv
+        for i, day in enumerate(self.dyn_eff_days):
+            dd = self.dyn_eff_day.week(week_num, silent=True)
+            pv = dd['pri']['vol'].sum()
+            sv = dd['sec']['vol'].sum()
+            dyn_eff_vol[i] = pv, sv, pv+sv
+        pri_vol = max_eff_vol['pri'] + dyn_eff_vol['sum']
+        sec_vol = dm['sec']['vol'].sum() + dd['sec']['vol'].sum()
+        net_vol = pri_vol + sec_vol
+        return pri_vol, sec_vol, net_vol
+        
+        
         
         
 if __name__ == '__main__':
     week_num = int(sys.argv[1])
+    day = sys.argv[2].upper() if len(sys.argv)>2 else 'ALL'
     BSQ = Lift('BACK SQ', 140, 20, 60)
     FSQ = Lift('FRONT SQ', 90, 12.5, 52.5)
     SDL = Lift('SUMO DL', 137.5, 20, 60)
@@ -133,17 +163,26 @@ if __name__ == '__main__':
     IBP = Lift('INCLINE BP', 52.5, 12.5, 32.5)
     OHP = Lift('OHP', 70, 12.5, 32.5)
 
-    DLMAX = MaxEffDay(SDL, CDL)
-    DLDYN = DynEffDay(SDL, BSQ)
-    BPMAX = MaxEffDay(FBP, IBP)
-    BPDYN = DynEffDay(FBP, OHP)
-    SQMAX = MaxEffDay(BSQ, FSQ)
-    SQDYN = DynEffDay(BSQ, SDL)
+    DLMAX = MaxEffDay('WEDNSDAY (Max Effort DL)', SDL, CDL)
+    DLDYN = DynEffDay('SUNDAY (Dynamic Effort DL)', SDL, BSQ)
+    BPMAX = MaxEffDay('FRIDAY (Max Effort BP)', FBP, IBP)
+    BPDYN = DynEffDay('SUNDAY (Dynamic Effort BP)', FBP, OHP)
+    SQMAX = MaxEffDay('SUNDAY (Max Effort SQ)', BSQ, FSQ)
+    SQDYN = DynEffDay('WEDNSDAY (Dynamic Effort SQ)', BSQ, SDL)
 
-    print('        WEDNSDAY (Max Effort DL)')
-    _ = DLMAX.week(week_num)
-    print('\n        FRIDAY (Max Effort BP)')
-    _ = BPMAX.week(week_num)
-    print('\n        SUNDAY (Dynamic Effort DL + BP)')
-    _ = DLDYN.week(week_num)
-    _ = BPDYN.week(week_num)
+    cycle = Micro([DLMAX], [DLDYN])
+
+    silent = dict(WED=True, FRI=True, SUN=True)
+    if day!='ALL':
+        silent[day[:3]]=False
+    else:
+        for day in silent.keys():
+            silent[day]=False
+            
+    # print('        WEDNSDAY (Max Effort DL)') 
+    dlmax_tbl = DLMAX.week(week_num, silent['WED'])
+    # print('\n        FRIDAY (Max Effort BP)') 
+    bpmax_tbl = BPMAX.week(week_num, silent['FRI'])
+    # print('\n        SUNDAY (Dynamic Effort DL + BP)')
+    dldyn_tbl = DLDYN.week(week_num, silent['SUN'])
+    bpdyn_tbl = BPDYN.week(week_num, silent['SUN'])
